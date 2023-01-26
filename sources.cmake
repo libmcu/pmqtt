14c291753babcd2cce22d7c97746df6043b878b2
Kyunghwan Kwon <k@mononn.com>
2023-01-26 16:35:30 +0900

    Fix CMake working directory

--- sources.cmake
+++ sources.cmake
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: MIT
 
 if(TARGET_PLATFORM STREQUAL coremqtt)
-	list(APPEND PMQTT_SRCS ${CMAKE_CURRENT_SOURCE_DIR}/src/coremqtt.c)
+	list(APPEND PMQTT_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/coremqtt.c)
 else()
 	message(WARN "No target specified. Building with dummy target")
-	list(APPEND PMQTT_SRCS ${CMAKE_CURRENT_SOURCE_DIR}/src/dummy.c)
+	list(APPEND PMQTT_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/dummy.c)
 endif()
 
-list(APPEND PMQTT_INCS ${CMAKE_CURRENT_SOURCE_DIR}/include)
+list(APPEND PMQTT_INCS ${CMAKE_CURRENT_LIST_DIR}/include)
